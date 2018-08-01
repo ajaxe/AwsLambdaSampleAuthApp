d5013db2e35da740a7ecf10f543223e7a22e617a
Ajay sharma <ajay.sharma1984@gmail.com>
2018-08-01 08:44:16 -0400

    [#4] initial ui setup. tooling: typescript, vue and parcel bundler. Starting with Login component.

---
+++ src/web/assets/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container align-self-start">
+                <span class="navbar-brand mb-0 h1">{{appName}}</span>
+            </div>
+        </nav>
+        <div class="container main-container">
+            <login />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.main-container {
+    margin-top: -100px;
+}
+nav {
+    height: 200px;
+}
+</style>
+
+
+<script lang="ts">
+import { Vue, Component, Prop, Provide } from "vue-property-decorator";
+import Login from './components/Login.vue';
+
+@Component({
+    components: {
+        Login
+    }
+})
+export default class App extends Vue {
+  @Provide() appName: string = "AWS Sample Lambda Web App";
+
+  sayHiBack(): void {
+    console.log("hi");
+  }
+}
+</script>
+
---
+++ src/web/assets/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="login" class="row justify-content-center">
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Login</h5>
+                    <form novalidate>
+                        <div class="form-group">
+                            <label for="username">Email address</label>
+                            <input type="email" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter email" required aria-required="true">
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="Password" required aria-required="true">
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-between">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="align-self-center">
+                            <a href="#">Register</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card-footer {
+  background-color: transparent;
+}
+</style>
+
+
+<script lang="ts">
+import { Vue, Component, Prop, Provide } from "vue-property-decorator";
+
+@Component
+export default class Login extends Vue {}
+</script>
+
